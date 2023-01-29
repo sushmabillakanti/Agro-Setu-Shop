d385edde7e30bb8b4c69c1b623daf3d372939f87
sushmabillakanti <sushma.billakanti15@gmail.com>
2023-01-29 18:28:32 +0530

    commit

--- agro_setu_shop/lib/Consumer/searchcartdesign.dart
+++ agro_setu_shop/lib/Consumer/searchcartdesign.dart
@@ -18,38 +18,21 @@ class _ProductsDesignWidgetState extends State<ProductsDesignWidget> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
+      child:
+      Padding(
         padding: const EdgeInsets.all(16.0),
         child: Container(
-          height: 170,
+          height: 60,
             width:MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              CircleAvatar(
-                backgroundColor: Colors.green,
-                minRadius: 50.0,
-                child: CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: NetworkImage(
-                    widget.model!.product_image!,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 15.0,),
-              Text(
-                widget.model!.product_name!,
-                style: const TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-              const SizedBox(height: 10.0,),
-              Text(
-                widget.model!.weight!,
-                style: const TextStyle(
-                  fontSize: 15.0,
-                ),
-              ),
-            ],
+          child: ListTile(
+            title: Text(widget.model!.product_name!,style: TextStyle(fontSize: 20.0),),
+            subtitle: Text(widget.model!.weight!),
+            leading: SizedBox(
+              width: 100,
+               height: 100,
+               child: Image.network(widget.model!.product_image!),
+          ),
+            trailing:const Icon(Icons.add_shopping_cart_outlined),
           ),
         ),
       ),
